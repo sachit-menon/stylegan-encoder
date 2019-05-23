d64d3aef24f2c7573f387a7cd1096a00991e3de2
sachit-menon <sm568@duke.edu>
2019-05-23 18:38:43 -0500

    added perceptual layer averaging, more tests

--- encoder/perceptual_model.py
+++ encoder/perceptual_model.py
@@ -13,8 +13,8 @@ def load_images(images_list, img_size):
         img = np.expand_dims(img, 0)
         loaded_images.append(img)
     loaded_images = np.vstack(loaded_images)
-    preprocessed_images = preprocess_input(loaded_images)
-    return preprocessed_images
+    # preprocessed_images = preprocess_input(loaded_images)
+    return loaded_images #preprocessed_images
 
 
 class PerceptualModel:
@@ -28,28 +28,76 @@ class PerceptualModel:
         self.perceptual_model = None
         self.ref_img_features = None
         self.features_weight = None
+
+        self.ref_img_features1 = None
+        self.features_weight1 = None
+
+        self.ref_img_features2 = None
+        self.features_weight2 = None
+
+        self.ref_img = None
+
+        self.nonperceptual_loss = None
+        self.perceptual_loss = None
         self.loss = None
 
     def build_perceptual_model(self, generated_image_tensor):
         vgg16 = VGG16(include_top=False, input_shape=(self.img_size, self.img_size, 3))
-        self.perceptual_model = Model(vgg16.input, vgg16.layers[self.layer].output)
-        generated_image = preprocess_input(tf.image.resize_images(generated_image_tensor,
-                                                                  (self.img_size, self.img_size), method=1))
-        generated_img_features = self.perceptual_model(generated_image)
+        self.perceptual_model = Model(vgg16.input, [vgg16.layers[self.layer].output, vgg16.layers[6].output, vgg16.layers[3].output])
+        generated_image = tf.image.resize_images(generated_image_tensor,
+                                                 (self.img_size, self.img_size), method=1)
 
-        self.ref_img_features = tf.get_variable('ref_img_features', shape=generated_img_features.shape,
+        preprocessed_generated_image = preprocess_input(generated_image)
+        generated_img_features = self.perceptual_model(preprocessed_generated_image)
+
+        self.ref_img_features = tf.get_variable('ref_img_features', shape=generated_img_features[0].shape,
                                                 dtype='float32', initializer=tf.initializers.zeros())
-        self.features_weight = tf.get_variable('features_weight', shape=generated_img_features.shape,
+        self.features_weight = tf.get_variable('features_weight', shape=generated_img_features[0].shape,
                                                dtype='float32', initializer=tf.initializers.zeros())
+
+        self.ref_img_features1 = tf.get_variable('ref_img_features1', shape=generated_img_features[1].shape,
+                                                dtype='float32', initializer=tf.initializers.zeros())
+        self.features_weight1 = tf.get_variable('features_weight1', shape=generated_img_features[1].shape,
+                                               dtype='float32', initializer=tf.initializers.zeros())
+
+        self.ref_img_features2 = tf.get_variable('ref_img_features2', shape=generated_img_features[2].shape,
+                                                 dtype='float32', initializer=tf.initializers.zeros())
+        self.features_weight2 = tf.get_variable('features_weight2', shape=generated_img_features[2].shape,
+                                                dtype='float32', initializer=tf.initializers.zeros())
+
+
+        self.ref_img = tf.get_variable('ref_img', shape=generated_image.shape,
+                                       dtype='float32', initializer=tf.initializers.zeros())
+
         self.sess.run([self.features_weight.initializer, self.features_weight.initializer])
 
-        self.loss = tf.losses.mean_squared_error(self.features_weight * self.ref_img_features,
-                                                 self.features_weight * generated_img_features) / 82890.0
+        self.sess.run([self.features_weight1.initializer, self.features_weight1.initializer])
+
+        self.sess.run([self.features_weight2.initializer, self.features_weight2.initializer])
+
+
+        self.perceptual_loss = tf.losses.mean_squared_error(self.features_weight * self.ref_img_features,
+                                                 self.features_weight * generated_img_features[0]) / 82890.0 \
+                               + tf.losses.mean_squared_error(self.features_weight1 * self.ref_img_features1,
+                                                            self.features_weight1 * generated_img_features[1]) / 82890.0 \
+                               + tf.losses.mean_squared_error(self.features_weight2 * self.ref_img_features2,
+                                                            self.features_weight2 * generated_img_features[2]) / 82890.0
+
+
+
+
+        self.nonperceptual_loss = tf.reduce_sum(tf.square(self.ref_img/255. - generated_image/255.))/10000.
+
+        lambda_per = 1
+        lambda_nonper = 0
+
+        self.loss = lambda_nonper*self.nonperceptual_loss + lambda_per*self.perceptual_loss
 
     def set_reference_images(self, images_list):
         assert(len(images_list) != 0 and len(images_list) <= self.batch_size)
         loaded_image = load_images(images_list, self.img_size)
-        image_features = self.perceptual_model.predict_on_batch(loaded_image)
+        preprocessed_image = preprocess_input(loaded_image)
+        image_features = self.perceptual_model.predict_on_batch(preprocessed_image)
 
         # in case if number of images less than actual batch size
         # can be optimized further
@@ -66,7 +114,15 @@ class PerceptualModel:
             image_features = np.vstack([image_features, np.zeros(empty_features_shape)])
 
         self.sess.run(tf.assign(self.features_weight, weight_mask))
-        self.sess.run(tf.assign(self.ref_img_features, image_features))
+        self.sess.run(tf.assign(self.features_weight1, weight_mask))
+        self.sess.run(tf.assign(self.features_weight2, weight_mask))
+
+
+        self.sess.run(tf.assign(self.ref_img_features, image_features[0]))
+        self.sess.run(tf.assign(self.ref_img_features1, image_features[1]))
+        self.sess.run(tf.assign(self.ref_img_features2, image_features[2]))
+
+        self.sess.run(tf.assign(self.ref_img, loaded_image))
 
     def optimize(self, vars_to_optimize, iterations=500, learning_rate=1.):
         vars_to_optimize = vars_to_optimize if isinstance(vars_to_optimize, list) else [vars_to_optimize]
